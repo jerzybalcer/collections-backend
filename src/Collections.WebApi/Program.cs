b7051ab3d311fc7841b88a2ed99bba01a840de94
jerzybalcer <jurekbalcer@gmail.com>
2022-11-15 20:49:30 +0100

    test cors added

--- src/Collections.WebApi/Program.cs
+++ src/Collections.WebApi/Program.cs
@@ -10,6 +10,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("localPolicy",
+                      builder =>
+                      {
+                          builder.WithOrigins("http://localhost:3000",
+                                              "http://localhost:7185");
+                      });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,6 +28,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("localPolicy");
 
 app.UseHttpsRedirection();
 
