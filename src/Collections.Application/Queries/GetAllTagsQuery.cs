724bddbf3c5ea68bd07c0c8b609504f2ea7fe8c5
jerzybalcer <jurekbalcer@gmail.com>
2022-11-26 23:39:31 +0100

    get all tags query

---
+++ src/Collections.Application/Queries/GetAllTagsQuery.cs
@@ -0,0 +1,26 @@
+﻿using Collections.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Collections.Application.Queries;
+
+public class GetAllTagsQuery : IRequest<List<string>>
+{
+    internal class GetAllTagsQueryHandler : IRequestHandler<GetAllTagsQuery, List<string>>
+    {
+        private readonly ICollectionsDbContext _dbContext;
+
+        public GetAllTagsQueryHandler(ICollectionsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<string>> Handle(GetAllTagsQuery request, CancellationToken cancellationToken)
+        {
+            return await _dbContext.Tags
+                .OrderBy(t => t.Name)
+                .Select(t => t.Name)
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
---
+++ src/Collections.WebApi/Controllers/TagController.cs
@@ -0,0 +1,26 @@
+﻿using Collections.Application.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Collections.WebApi.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class TagController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TagController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("tags")]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            var result = await _mediator.Send(new GetAllTagsQuery());
+
+            return Ok(result);
+        }
+    }
+}
